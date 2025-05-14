89be2bfe754f969901f3c5068661119825128035
Dimitri Igoshev <61008146+Dimitri-Igoshev@users.noreply.github.com>
2025-05-14 20:41:03 +0300

    http

--- client/common/env.ts
+++ client/common/env.ts
@@ -2,6 +2,6 @@ export const config = {
   // API_URL: "http://localhost:4000/api",
   // MEDIA_URL: "http://localhost:4000",
 
-  API_URL: "https://blow.ru/api",
-  MEDIA_URL: "https://blow.ru",
+  API_URL: "http://blow.ru/api",
+  MEDIA_URL: "http://blow.ru",
 };
