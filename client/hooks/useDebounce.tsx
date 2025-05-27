f7249df0f71dc8e3e3b73f97e817ff3d2031d2c4
Dimitri Igoshev <61008146+Dimitri-Igoshev@users.noreply.github.com>
2025-05-28 01:00:21 +0300

    Update useDebounce.tsx

--- client/hooks/useDebounce.tsx
+++ client/hooks/useDebounce.tsx
@@ -1,1 +1,21 @@
-useDebounce
+// "use effect";
+
+// import { useState, useEffect } from "react";
+
+// export default function useDebounce(value: any, delay: any) {
+//   const [debouncedValue, setDebouncedValue] = useState(value);
+
+//   useEffect(() => {
+//     if (!value) return;
+
+//     const handler = setTimeout(() => {
+//       setDebouncedValue(value);
+//     }, delay);
+
+//     return () => {
+//       clearTimeout(handler);
+//     };
+//   }, [value]);
+
+//   return debouncedValue;
+// }
