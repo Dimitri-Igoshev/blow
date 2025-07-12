8f744368522b79584f52b717540aa1cae5d89ced
Dimitri Igoshev <61008146+Dimitri-Igoshev@users.noreply.github.com>
2025-07-12 17:32:53 +0300

    Update route.ts

--- client/app/api/payment/route.ts
+++ client/app/api/payment/route.ts
@@ -61,13 +61,55 @@ export async function POST(req: NextRequest) {
 		// const authHeader =
 		//   "Basic " + Buffer.from(`${username}:${password}`).toString("base64");
 
+		const paymentData = {
+  TerminalKey: "1752254920336DEMO",
+  Amount: 19200,
+  OrderId: "21090",
+  Description: "Подарочная карта на 1000 рублей",
+  Token: "a202c5ac3dca5b52ef8fbee86fc44254dd39e78f2553863ffac9c7efe0f3c9e5",
+  DATA: {
+    Phone: "+71234567890",
+    Email: "a@test.com",
+  },
+  Receipt: {
+    Email: "a@test.ru",
+    Phone: "+79031234567",
+    Taxation: "osn",
+    Items: [
+      {
+        Name: "Наименование товара 1",
+        Price: 10000,
+        Quantity: 1,
+        Amount: 10000,
+        Tax: "vat10",
+        Ean13: "303130323930303030630333435",
+      },
+      {
+        Name: "Наименование товара 2",
+        Price: 3500,
+        Quantity: 2,
+        Amount: 7000,
+        Tax: "vat20",
+      },
+      {
+        Name: "Наименование товара 3",
+        Price: 550,
+        Quantity: 4,
+        Amount: 2200, // 550 * 4, было 4200 — ошибка, если не опечатка
+        Tax: "vat10",
+      },
+    ],
+  },
+};
+
+
 		const res = await fetch("https://rest-api-test.tinkoff.ru/v2/Init", {
 			method: "POST",
 			// headers: {
 			//   Authorization: authHeader,
 			//   "Content-Type": "application/json",
 			// },
-			body: JSON.stringify(rest),
+			body: JSON.stringify(paymentData),
 		});
 
 		if (!res.ok) {
