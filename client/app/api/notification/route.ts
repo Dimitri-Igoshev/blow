0db8166b9cc530e2b61781ad9d9e7778b09017ec
Dimitri Igoshev <61008146+Dimitri-Igoshev@users.noreply.github.com>
2025-07-13 21:33:11 +0300

    Update route.ts

--- client/app/api/notification/route.ts
+++ client/app/api/notification/route.ts
@@ -4,58 +4,45 @@ import { NextRequest } from "next/server";
 
 import { IncomingPayload } from "../types";
 
-export async function POST(req: NextRequest) {
-	try {
-		const data: IncomingPayload = await req.json();
+export async function POST(req: NextRequest): Promise<Response> {
+  try {
+    const data: IncomingPayload = await req.json();
 
-		console.log("Полученные данные от внешнего API:", data);
+    console.log('Получены данные от внешнего API:', data);
 
-		// Пример: делаем какие-то дополнительные действия с полученными данными
-		// Например, можно добавить какое-то поле или изменить данные
-		// const processedData = {
-		//   ...data,
-		//   receivedAt: new Date().toISOString(), // Добавим время получения
-		//   status: 'processed', // Статус обработки
-		// };
+    // Отправляем внутреннее уведомление
+    const result = await fetch('https://blow.igoshev.de/api/payment/notification', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ orderId: data.OrderId, status: data.Status }),
+    });
 
-		// Возвращаем результат обработки данных
-		// return Response.json({
-		//   status: 'success',
-		//   message: 'Данные успешно получены и обработаны!',
-		//   processedData,
-		// });
+    if (result.ok) {
+      return new Response('OK', {
+        status: 200,
+        headers: {
+          'Content-Type': 'text/plain',
+        },
+      });
+    }
 
-		const result = await fetch(
-			`https://blow.igoshev.de/api/payment/notification`,
-			{
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({ orderId: data.OrderId, status: data.Status }),
-			}
-		);
+    // В случае ошибки от внутреннего API
+    return new Response(JSON.stringify({ message: 'Ошибка сервера при зачислении средств' }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    console.error('[WEBHOOK ERROR]', error);
 
-		// const result = await response.json();
-
-		// console.log('[NOTIFICATION RESPONSE]', result);
-		if (result.status === 200) {
-			return new Response("OK", {
-				status: 200,
-				headers: {
-					"Content-Type": "text/plain",
-				},
-			});
-		}
-
-		return new Response(
-			JSON.stringify({ message: "Ошибка сервера при зачислении средств" })
-		);
-	} catch (error) {
-		console.error("[WEBHOOK ERROR]", error);
-
-		return new Response(JSON.stringify({ message: "Ошибка сервера" }), {
-			status: 500,
-		});
-	}
+    return new Response(JSON.stringify({ message: 'Ошибка сервера' }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
 }
