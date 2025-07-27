b83af458635dd48ef203410db988825924483413
Dimitri Igoshev <61008146+Dimitri-Igoshev@users.noreply.github.com>
2025-07-27 13:26:08 +0300

    2 cards

--- client/app/account/search/page.tsx
+++ client/app/account/search/page.tsx
@@ -41,7 +41,7 @@ const AccountSearch = () => {
       </div>
       {users?.length ? (
         <>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 gap-5 sm:gap-[34px] mt-[16px] sm:mt-0">
+          <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 gap-5 sm:gap-[34px] mt-[16px] sm:mt-0">
             {users.map((item: any) => (
               <ProfilePreview key={item._id} item={item} />
             ))}
--- client/components/ProfilePreview.tsx
+++ client/components/ProfilePreview.tsx
@@ -1,9 +1,9 @@
 import { FC, useRef } from "react";
 import { cn } from "@heroui/theme";
-import { Image } from "@heroui/image";
 import { Button } from "@heroui/button";
 import { PiWaveform } from "react-icons/pi";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 import { config } from "@/common/env";
 // import { getCityString } from "@/helper/getCityString";
@@ -13,7 +13,7 @@ import { isPremium, isTop } from "@/helper/checkIsActive";
 import { useGetMeQuery } from "@/redux/services/userApi";
 import { InfoModal } from "./InfoModal";
 import { useDisclosure } from "@heroui/react";
-import { useCityLabel } from "@/helper/getCityString"
+import { useCityLabel } from "@/helper/getCityString";
 
 interface ProfilePreviewProps {
 	item: any;
@@ -46,12 +46,12 @@ export const ProfilePreview: FC<ProfilePreviewProps> = ({
 	const handlePlay = () => {
 		if (!me || me?.status !== "active") {
 			onRegistrationRequired();
-			return
+			return;
 		}
 
 		if (!premium && me?.sex === "male") {
 			onPremiumRequired();
-			return
+			return;
 		}
 
 		const audio = new Audio(`${config.MEDIA_URL}/${item?.voice}`);
@@ -60,42 +60,43 @@ export const ProfilePreview: FC<ProfilePreviewProps> = ({
 		});
 	};
 
-	const { getCityLabel } = useCityLabel()
+	const { getCityLabel } = useCityLabel();
 
 	return (
 		<>
 			<div
 				className={cn(
-					"bg-white dark:bg-foreground-100 p-6 flex flex-col xl:flex-row gap-6 rounded-[32px] col-span-1 w-full",
+					"relative bg-white dark:bg-foreground-100 p-3 sm:p-6 flex flex-col xl:flex-row gap-3 sm:gap-6 rounded-[32px] col-span-1 w-full pb-[60px] sm:pb-0",
 					className
 				)}
 			>
 				<button
-					className="overflow-hidden relative rounded-[20px] min-h-[345px] min-w-[230px] cursor-pointer"
+					className="relative w-full aspect-[2/3] overflow-hidden rounded-[20px] cursor-pointer"
 					onClick={() => router.push(ROUTES.ACCOUNT.SEARCH + "/" + item?._id)}
 				>
 					<Image
 						alt=""
-						className="z-0 relative w-full sm:w-auto h-full xl:h-[345px] xl:w-[230px] object-cover"
-						radius="none"
 						src={
-							item?.photos[0]?.url
+							item?.photos?.[0]?.url
 								? `${config.MEDIA_URL}/${item.photos[0].url}`
 								: item?.sex === "male"
 									? "/men2.png"
 									: "/woman2.png"
 						}
+						fill
+						className="object-cover"
 					/>
-					<div className="bg-transparent absolute -right-[70px] top-[20px] z-0">
-						{isTop(item) ? (
-							<div className="bg-primary w-[200px] h-[30px] rotate-45 flex justify-center items-center">
-								<Image alt="" className="w-[40px]" src="/top.png" />
+					{/* ТОП лейбл */}
+					{isTop(item) && (
+						<div className="absolute -right-[70px] top-[20px] z-10">
+							<div className="bg-primary w-[133px] h-[20px] sm:w-[200px] sm:h-[30px] rotate-45 flex justify-center items-center">
+								<Image alt="" className="w-[27px] sm:w-[40px]" src="/top.png" />
 							</div>
-						) : null}
-					</div>
+						</div>
+					)}
 				</button>
 
-				<div className="flex flex-col justify-between gap-6 w-full">
+				<div className="flex flex-col justify-between gap-3 sm:gap-6 w-full">
 					<div className="flex flex-col gap-1 mt-1">
 						<div className="flex gap-1.5 items-center">
 							{getActivityString(item?.activity) === "онлайн" ? (
@@ -110,15 +111,15 @@ export const ProfilePreview: FC<ProfilePreviewProps> = ({
 							)}
 						</div>
 
-						<div className="flex justify-between items-center">
-							<p className="text-[24px] font-semibold">
+						<div className="flex justify-between items-center flex-wrap">
+							<p className="text-[16px] sm:text-[24px] font-semibold w-full sm:w-auto">
 								{item?.firstName
 									? item.firstName
 									: item.sex === "male"
 										? "Мужчина"
 										: "Девушка"}
 							</p>
-							<p>
+							<p className="text-[12px] sm:text-base">
 								{item?.age ? item.age + ", " : ""}
 								{getCityLabel(item?.city)}
 							</p>
@@ -126,16 +127,18 @@ export const ProfilePreview: FC<ProfilePreviewProps> = ({
 					</div>
 
 					<div className="flex flex-col gap-1">
-						<p className="font-semibold">Данные</p>
-						<p className="text-[14px]">
-							рост - {item?.height} см{" "}
-							<span className="ml-3">вес - {item?.weight} кг</span>
-						</p>
+						<p className="font-semibold text-[14px] sm:text-base">Данные</p>
+						<div className="text-[14px] flex flex-wrap gap-1">
+							<p className="w-full sm:w-auto">рост - {item?.height} см,</p>
+							<p>вес - {item?.weight} кг</p>
+						</div>
 					</div>
 
 					<div className="flex flex-col gap-1">
-						<p className="font-semibold">Цели знакомства</p>
-						<ul className="list-disc leading-5 ml-4 text-[14px]">
+						<p className="font-semibold text-[12px] sm:text-base">
+							Цели знакомства
+						</p>
+						<ul className="list-disc leading-5 ml-4 text-[12px] sm:text-[14px]">
 							{item.sponsor ? (
 								<li>
 									{item?.sex === "male" ? "стану спонсором" : "ищу спонсора"}
@@ -147,20 +150,26 @@ export const ProfilePreview: FC<ProfilePreviewProps> = ({
 						</ul>
 					</div>
 
-					<div className="grid grid-cols-5 gap-3 w-full items-end">
-						<div className="col-span-3">
-							{item?.voice ? (
+					<div className="absolute sm:relative p-3 sm:p-0 bottom-0 left-0 right-0 mt-auto flex flex-row sm:justify-end sm:items-center sm:gap-3 gap-2 w-full">
+						{/* Кнопка "Голос", если есть */}
+						{item?.voice && (
+							<>
 								<button
 									onClick={handlePlay}
-									className="bg-primary text-white rounded-full h-[38px] px-3.5 flex gap-1 items-center"
+									className="w-full hidden sm:flex sm:w-auto justify-center bg-primary text-white rounded-full h-[38px] px-3.5 flex gap-1 items-center"
 								>
 									<PiWaveform className="w-5 h-5" />
 									<p>Голос</p>
 								</button>
-							) : null}
-						</div>
+								<Button isIconOnly color="primary" radius="full" className="flex sm:hidden" onPress={handlePlay}>
+									<PiWaveform className="w-5 h-5" />
+								</Button>
+							</>
+						)}
+
+						{/* Кнопка "Профиль" */}
 						<Button
-							className="w-full z-0 relative col-span-2"
+							className="w-full sm:w-auto flex"
 							color="secondary"
 							radius="full"
 							variant="solid"
