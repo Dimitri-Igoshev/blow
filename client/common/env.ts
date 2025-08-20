6652c3f871ceb435e6e9e7bb351ab9dc9838041f
Dimitri Igoshev <61008146+Dimitri-Igoshev@users.noreply.github.com>
2025-08-20 21:38:34 +0300

    Update env.ts

--- client/common/env.ts
+++ client/common/env.ts
@@ -5,11 +5,11 @@ export const config = {
 	// API_URL: "https://blow.ru/api",
 	// MEDIA_URL: "https://blow.ru",
 
-	// API_URL: "https://blow.igoshev.de/api",
-	// MEDIA_URL: "https://blow.igoshev.de",
+	API_URL: "https://blow.igoshev.de/api",
+	MEDIA_URL: "https://blow.igoshev.de",
 
-	API_URL: "https://api.blow.ru/api",
-	MEDIA_URL: "https://api.blow.ru",
+	// API_URL: "https://api.blow.ru/api",
+	// MEDIA_URL: "https://api.blow.ru",
 
 	TBANK_TERMINAL_KEY: "1752254920585",
 	TBANK_PASSWORD: "k%mOHw64FH5Ex&vF",
