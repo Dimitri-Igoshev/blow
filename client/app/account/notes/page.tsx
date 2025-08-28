2792fa6582e4d89ecda1d4187ddcf653488e89fc
Dimitri Igoshev <61008146+Dimitri-Igoshev@users.noreply.github.com>
2025-08-28 13:07:05 +0300

    Update page.tsx

--- client/app/account/notes/page.tsx
+++ client/app/account/notes/page.tsx
@@ -14,7 +14,7 @@ export default function AccountNotes() {
 	};
 
 	return (
-		<div className="flex w-full flex-col px-9 min-h-screen pt-[84px] gap-[30px]">
+		<div className="flex w-full flex-col px-3 sm:px-9 min-h-screen pt-[84px] gap-[30px]">
 			<div className="flex w-full items-center justify-between">
 				<h1 className="font-semibold text-[36px]">Заметки</h1>
 			</div>
