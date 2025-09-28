6bd8071773c832eedc231683986ab0cc6ad1f66f
Dimitri Igoshev <61008146+Dimitri-Igoshev@users.noreply.github.com>
2025-09-28 21:57:05 +0200

    girls

--- client/app/promo/for-mens/page.tsx
+++ client/app/promo/for-mens/page.tsx
@@ -1,17 +1,17 @@
 'use client'
 
 import { PromoBgRightSlide } from "../components/PromoBgRightSlide"
-import { PromoBgTopDown } from "../components/PromoBgTopDown"
 import { PromoHeader } from "../components/PromoHeader"
 import { PromoHero } from "../components/PromoHero"
 import { PromoNext } from "../components/PromoNext"
+import { profiles } from "../data/girls"
 
 export default function PromoForMens() {
   return (
     <div className="min-h-screen w-full bg-black text-white relative overflow-hidden">
       <PromoBgRightSlide />
       <PromoHeader />
-      <PromoHero />
+      <PromoHero profiles={profiles} />
       <PromoNext />
     </div>
   )
--- client/app/promo/for-womans/page.tsx
+++ client/app/promo/for-womans/page.tsx
@@ -1,18 +1,18 @@
 'use client'
 
 import { PromoBgRightSlide } from "../components/PromoBgRightSlide"
-import { PromoBgTopDown } from "../components/PromoBgTopDown"
 import { PromoHeader } from "../components/PromoHeader"
 import { PromoHero } from "../components/PromoHero"
 import { PromoNext } from "../components/PromoNext"
+import { profiles } from "../data/mens"
 
 export default function PromoForMens() {
   return (
     <div className="min-h-screen w-full bg-black text-white relative overflow-hidden">
       <PromoBgRightSlide />
       <PromoHeader />
-      <PromoHero />
-      <PromoNext />
+      <PromoHero profiles={profiles} sex="female" />
+      <PromoNext sex="female" />
     </div>
   )
 }
